3c6b7fbb11d58ed515b5bbd4049f7c665c6078da
danielmschaves <danielmschaves@gmail.com>
2024-06-18 14:20:47 -0300

    feature: bigquery ecommerce data ingestion

--- ingestion/duck.py
+++ ingestion/duck.py
@@ -30,13 +30,11 @@ def write_to_s3_from_duckdb(
     duckdb_con.sql(
         f"""
         COPY (
-            SELECT *,
-                YEAR({timestamp_column}) AS year, 
-                MONTH({timestamp_column}) AS month 
+            SELECT *
             FROM {table}
         ) 
         TO '{s3_path}/{table}' 
-        (FORMAT PARQUET, PARTITION_BY (year, month), OVERWRITE_OR_IGNORE 1, COMPRESSION 'ZSTD', ROW_GROUP_SIZE 1000000);
+        (FORMAT PARQUET);
     """
     )
 
