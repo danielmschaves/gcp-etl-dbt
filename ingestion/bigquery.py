102c7a395eddae79cb9a3cd3cb8e00fd736530d0
danielmschaves <danielmschaves@gmail.com>
2024-06-18 17:04:50 -0300

    feature: dinamic query in gcp-table creation in duckdb

--- ingestion/bigquery.py
+++ ingestion/bigquery.py
@@ -3,21 +3,49 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from google.auth.exceptions import DefaultCredentialsError
 from loguru import logger
+from typing import List
 import time
-from models import EcommerceJobParameters  # Make sure this import points to the right location
+from models import EcommerceJobParameters 
 import pandas as pd
 
 ECOMMERCE_PUBLIC_DATASET = "bigquery-public-data.thelook_ecommerce"
 
-def build_ecommerce_query(params: EcommerceJobParameters, table_name: str, ecom_public_dataset: str = ECOMMERCE_PUBLIC_DATASET) -> str:
-    """Generate SQL to query a specific table based on provided parameters."""
-    if table_name in params.table_names:
-        return f"SELECT * FROM `{ecom_public_dataset}.{table_name}`"
-    else:
-        return None  # Return None if the table name is not in the list or invali
+def build_ecommerce_query(params: EcommerceJobParameters, ecom_public_dataset: str = ECOMMERCE_PUBLIC_DATASET) -> List[str]:
+    """
+    Generate SQL queries to query specific tables based on provided parameters.
+
+    Args:
+        params (EcommerceJobParameters): The parameters for the Ecommerce job.
+        ecom_public_dataset (str, optional): The name of the Ecommerce public dataset. Defaults to ECOMMERCE_PUBLIC_DATASET.
+
+    Returns:
+        List[str]: A list of SQL queries.
+
+    """
+    queries = []
+    for table_name in params.table_names:
+        if table_name:
+            query = f"SELECT * FROM `{ecom_public_dataset}.{table_name}`"
+            queries.append(query)
+        else:
+            logger.warning(f"Invalid table name provided: {table_name}")
+    return queries
 
 def get_bigquery_client(project_name: str) -> bigquery.Client:
-    """Get BigQuery client"""
+    """
+    Get BigQuery client.
+
+    Args:
+        project_name (str): The name of the BigQuery project.
+
+    Returns:
+        bigquery.Client: The BigQuery client object.
+
+    Raises:
+        EnvironmentError: If no valid credentials are found for BigQuery authentication.
+        DefaultCredentialsError: If there is an error with the default credentials.
+
+    """
     try:
         service_account_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
         if service_account_path:
@@ -27,15 +55,28 @@ def get_bigquery_client(project_name: str) -> bigquery.Client:
     except DefaultCredentialsError as creds_error:
         raise creds_error
     
-def get_bigquery_result(query_str: str, bigquery_client: bigquery.Client) -> pd.DataFrame:
-    """Execute the query and return the results as a pandas DataFrame."""
-    try:
-        logger.info(f"Running query: {query_str}")
-        start_time = time.time()
-        dataframe = bigquery_client.query(query_str).to_dataframe()
-        elapsed_time = time.time() - start_time
-        logger.info(f"Query executed and data loaded in {elapsed_time:.2f} seconds")
-        return dataframe
-    except Exception as e:
-        logger.error(f"Error running query: {e}")
-        raise
+def get_bigquery_results(queries: List[str], table_names: List[str], bigquery_client: bigquery.Client) -> dict:
+    """
+    Execute queries and return results as a dictionary of pandas DataFrames.
+
+    Args:
+        queries (List[str]): A list of SQL queries to execute.
+        table_names (List[str]): A list of table names corresponding to each query.
+        bigquery_client (bigquery.Client): The BigQuery client object used to execute the queries.
+
+    Returns:
+        dict: A dictionary where the keys are table names and the values are pandas DataFrames containing the query results.
+    """
+    dataframes = {}
+    for query, table_name in zip(queries, table_names):
+        try:
+            logger.info(f"Running query for table: {table_name}")
+            start_time = time.time()
+            df = bigquery_client.query(query).to_dataframe()
+            elapsed_time = time.time() - start_time
+            logger.info(f"Query for {table_name} executed and data loaded in {elapsed_time:.2f} seconds")
+            dataframes[table_name] = df 
+        except Exception as e:
+            logger.error(f"Error running query for {table_name}: {e}")
+            raise
+    return dataframes  
--- ingestion/duck.py
+++ ingestion/duck.py
@@ -2,17 +2,48 @@ from typing import List
 from loguru import logger
 
 
-def create_table_from_dataframe(duckdb_con, table_name: str, dataframe: str):
-    duckdb_con.sql(
-        f"""
-        CREATE TABLE {table_name} AS 
-            SELECT *
-            FROM {dataframe}
-        """
-    )
+def create_table_from_dataframes(duckdb_con, dataframes: dict):
+    """
+    Create tables from a dictionary of DataFrame objects in DuckDB.
+
+    Parameters:
+    - duckdb_con: The DuckDB connection object.
+    - dataframes: A dictionary containing table names as keys and DataFrame objects as values.
+
+    Returns:
+    None
+
+    Raises:
+    - Exception: If there is an error while creating a table in DuckDB.
+
+    """
+    for table_name, dataframe in dataframes.items():
+        try:
+            # Create table in DuckDB
+            duckdb_con.sql(
+                f"""
+                CREATE TABLE {table_name} AS 
+                    SELECT * 
+                    FROM dataframe
+                    """, {'dataframe': dataframe}
+                    )
+            logger.info(f"Table {table_name} created successfully in DuckDB")
+        except Exception as e:
+            logger.error(f"Error while creating table {table_name} in DuckDB: {e}")
+            raise
 
 
 def connect_to_md(duckdb_con, motherduck_token: str):
+    """
+    Connects to the Mother Duck database using the provided DuckDB connection and Mother Duck token.
+
+    Args:
+        duckdb_con (DuckDBConnection): The DuckDB connection object.
+        motherduck_token (str): The token for authenticating with the Mother Duck database.
+
+    Returns:
+        None
+    """
     duckdb_con.sql(f"INSTALL md;")
     duckdb_con.sql(f"LOAD md;")
     duckdb_con.sql(f"SET motherduck_token='{motherduck_token}';")
@@ -20,12 +51,33 @@ def connect_to_md(duckdb_con, motherduck_token: str):
 
 
 def load_aws_credentials(duckdb_con, profile: str):
+    """
+    Loads AWS credentials for the specified profile into the DuckDB connection.
+
+    Parameters:
+    - duckdb_con: DuckDB connection object
+    - profile: AWS profile name
+
+    Returns:
+    None
+    """
     duckdb_con.sql(f"CALL load_aws_credentials('{profile}');")
 
 
 def write_to_s3_from_duckdb(
-    duckdb_con, table: str, s3_path: str, timestamp_column: str
+    duckdb_con, table: str, s3_path: str
 ):
+    """
+    Writes data from DuckDB to S3.
+
+    Args:
+        duckdb_con: The DuckDB connection object.
+        table (str): The name of the table to write.
+        s3_path (str): The S3 path to write the data to.
+
+    Returns:
+        None
+    """
     logger.info(f"Writing data to S3 {s3_path}/{table}")
     duckdb_con.sql(
         f"""
@@ -43,19 +95,24 @@ def write_to_md_from_duckdb(
     duckdb_con,
     table: str,
     local_database: str,
-    remote_database: str,
-    timestamp_column: str,
-    start_date: str,
-    end_date: str,
+    remote_database: str
 ):
+    """
+    Writes data from a DuckDB table to Motherduck.
+
+    Args:
+        duckdb_con: The DuckDB connection object.
+        table (str): The name of the table to write data from.
+        local_database (str): The name of the local database.
+        remote_database (str): The name of the remote database.
+
+    Returns:
+        None
+    """
     logger.info(f"Writing data to motherduck {remote_database}.main.{table}")
     duckdb_con.sql(f"CREATE DATABASE IF NOT EXISTS {remote_database}")
     duckdb_con.sql(
         f"CREATE TABLE IF NOT EXISTS {remote_database}.{table} AS SELECT * FROM {local_database}.{table} limit 0"
-    )
-    # Delete any existing data in the date range
-    duckdb_con.sql(
-        f"DELETE FROM {remote_database}.main.{table} WHERE {timestamp_column} BETWEEN '{start_date}' AND '{end_date}'"
     )
     # Insert new data
     duckdb_con.sql(
